5ca1afe69771f2d8cf196abf77a6dd8fdd0a40ec
laxmanindia <laxman091@gmail.com>
2018-05-06 10:09:20 +0530

    laxmanoffice branch commit

--- custom-checkout.php
+++ custom-checkout.php
@@ -17,9 +17,7 @@ wp_head();
 <body>
 
 <div class="container">
-  <h2>Checkout</h2>
-  
-
+  <h2>Checkout123</h2>
   
   <ul class="nav nav-tabs">
     <li class="active"><a data-toggle="tab" id="tab_login" href="#login">Login</a></li>
